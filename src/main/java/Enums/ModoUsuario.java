9ed5fc5287d76b020d9929a5e2fd877af0ab70dc
Andres Salgueiro (U) <andres.salgueiro@correo.unimet.edu.ve>
2025-11-08 12:48:12 -0400

    Primer Commit

---
+++ src/main/java/Enums/ModoUsuario.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Enums;
+
+/**
+ *
+ * @author Andres Salgueiro
+ */
+public enum ModoUsuario {
+    KERNEL, //Admin, SuperUsuario...etc.
+    USUARIO,
+}
---
+++ src/main/java/GestionIO/Planificador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestionIO;
+
+/**
+ *
+ * @author Andres Salgueiro
+ */
+public class Planificador {
+    //Aqui es donde se manejan las politicas
+}
